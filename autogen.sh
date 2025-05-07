edfcb81fbe845766d5592e443a289bd3f15b0efa
kryptoin <rdiapeter@gmail.com>
2025-05-07 13:48:29 -0700

    updated autogen

--- autogen.sh
+++ autogen.sh
@@ -1,35 +1,8 @@
 #!/bin/sh
 # Copyright (c) 2013-2016 The Bitcoin Core developers
+# Copyright (c) 2025 The LightningCash-R developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
-# set -e
-# srcdir="$(dirname $0)"
-# cd "$srcdir"
-# if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
-#   LIBTOOLIZE="${GLIBTOOLIZE}"
-#   export LIBTOOLIZE
-# fi
-# which autoreconf >/dev/null || \
-#   (echo "configuration failed, please install autoconf first" && exit 1)
-# autoreconf --install --force --warnings=all
-
-
-# export LC_ALL=C
-# set -e
-# srcdir="$(dirname "$0")"
-# cd "$srcdir"
-# if [ -z "${LIBTOOLIZE}" ] && GLIBTOOLIZE="$(command -v glibtoolize)"; then
-#   LIBTOOLIZE="${GLIBTOOLIZE}"
-#   export LIBTOOLIZE
-# fi
-# command -v autoreconf >/dev/null || \
-#   (echo "configuration failed, please install autoconf first" && exit 1)
-# autoreconf --install --force --warnings=all
-
-
-#!/bin/sh
-# More robust autogen.sh script using modern Autotools
 
 set -e
 
@@ -79,14 +52,8 @@ else
      echo "Warning: autopoint (from gettext) not found. Internationalization support might be incomplete."
 fi
 
-
 echo "Running autoreconf to generate build system files..."
 
-# Run autoreconf with install, force, warnings, and verbose flags
-# --install (-i): Install missing Autotools auxiliary files
-# --force (-f): Remake even if input files are older than output files
-# --warnings=all: Report all warnings
-# --verbose (-v): Print the name of each directory examined and commands run
 autoreconf --install --force --warnings=all --verbose
 
 echo "Autogen complete. You should now be able to run ./configure"
