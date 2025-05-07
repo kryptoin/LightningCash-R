a273cf91e099f659867c5e1df4f998bbf0d10d2c
kryptoin <rdiapeter@gmail.com>
2025-05-07 14:06:06 -0700

    updated license

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Copyright (c) 2013-2016 The Bitcoin Core developers
-# Copyright (c) 2025 The LightningCash-R developers
+# Copyright (c) 2025 The LightningCash-R Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
